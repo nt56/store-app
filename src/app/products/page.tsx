58b747b873f94e19759aec93eb1afbbd70b7fad7
nt_56 <nagutirth2121@gmail.com>
2025-06-06 11:49:34 +0530

    day-3 task done

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,15 @@
+import ProductsContainer from "@/components/products/ProductsContainer";
 import React from "react";
 
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+const ProductsPage = ({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) => {
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+
+  return <ProductsContainer layout={layout} search={search} />;
 };
 
 export default ProductsPage;
--- src/components/home/HeroCarousel.tsx
+++ src/components/home/HeroCarousel.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import hero1 from "../../../public/images/hero-1.jpg";
+import hero2 from "../../../public/images/hero-2.jpg";
+import hero3 from "../../../public/images/hero-3.jpg";
+import hero4 from "../../../public/images/hero-4.jpg";
+import hero5 from "../../../public/images/hero-5.jpg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+
+const carouselImages = [hero1, hero2, hero3, hero4, hero5];
 
 const HeroCarousel = () => {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="p-2">
+                    <Image
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroCarousel;
